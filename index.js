54c594a3045db412f18bc1695cbc319f9d049675
Manish <vishwamanish@gmail.com>
2021-12-22 18:26:49 +0530

    Add comments, password encryption and login function

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+//importing user model
 const User = require('./models/user');
 
-
+//connecting to database using url(DATABASE_URL) written in dotenv file
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
 });
 const db = mongoose.connection
+//if database has error in connection then showing error in console
 db.on('error',error => console.error(error))
+//once database connected then showing log in console
 db.once('open',() => console.log('Connected to Mongoose'))
 
 
@@ -24,20 +27,63 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
+//request for data on home page
 app.get('/',(req,res)=>{
     res.json({"hollo ~":"Hi ~~"})
 });
 
+//request for signUp or registeration
 app.post('/api/users/register',(req,res)=>{
-    const user = new User(req.body)
-    user.save((err,userData)=>{
+    //new userSchema for data given during signUp
+    const user = new User(req.body);
+
+    /-- before saving data we have to encrypt password that is done in user.js --/
+
+    //saving data to database with password encrypted
+    user.save((err,doc)=>{
+        //saving data got error(like error return in password encryption) then showing error
         if(err) return res.json({success:false,err})
-        return res.status(200).json({
-            success:true
+        //successful signUp and showing signUp data with status code 200
+        res.status(200).json({
+            success:true,
+            userData: doc
         });
-    });
-    
+    });  
 })
 
+//request for signIn or login
+app.post('/api/user/login',(req,res)=>{
+    //finding the email in database
+    User.findOne({email:req.body.email},(err,user)=>{
+        //if not able to find email then showing message
+        if(!user)
+        return res.json({
+            loginSuccess:false,
+            message:"Auth failed, email not found"
+        });
+
+        //if email got then comparing password, before this we make comparePassword method in user.js
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            //password is not matched then showing massage
+            if(!isMatch){
+                return res.json({loginSuccess:false, message:"wrong password"})
+            }
+        })
+
+        //JWT used to share security info between two parties-a client and a server
+        //generating Token to ensure that claim cannot be altered, before this we make generateToken method in user.js
+        // [user.generateToken] method then send the token as response to browser using cookie-parser.
+        user.generateToken((err, user)=>{
+            //error in generating token sending error
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(400)
+                .json({
+                    loginSuccess:true
+                })
+        })
+    })
+})
+
+//connecting to port written in dotenv file otherwise to 3000
 app.listen(process.env.PORT || 3000)
--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+//used in encryption of password
+const bcrypt = require('bcrypt');
+//password size is 10
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
+//schema that how the post request to be stored in database
 const userSchema = mongoose.Schema({
-
     name:{
         type:String,
         maxlength:50
@@ -30,6 +35,59 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+//before saving the data given, running [function]
+userSchema.pre('save',function(next){
+    //whatever data given before saving, transfering to [user]
+    var user = this;
+
+    if(user.isModified('password')){
+
+        //generating random salt(10 character string) with function
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            //error in generating salt, returning nextFunction with that error
+            if(err) return next(err);
+    
+            //successful in generating salt
+            //By hashing a plain text password plus a salt, the hash algorithm’s output is no longer predictable. The same password will no longer yield the same hash. The salt gets automatically included with the hash, so you do not need to store it in a database.
+            bcrypt.hash(user.password, salt, function(err, hash){
+                //error in hash algorithm, returning nextFunction with that error
+                if(err) return next(err);
+                //successfully storing hashed password in password section
+                user.password = hash
+            })
+        })
+    }else{
+        next()
+    }
+    
+})
+
+//making mathod --similarity [plainPassword=req.body.password],[cb=(err,isMatch)=>{}]
+userSchema.methods.comparePassword =  function(plainPassword,cb){
+    //comparing givenPassword to storedPassword
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        //if not matched then sending error to cb
+        if(err) return cb(err);
+        //if password matched then sending true to cb
+        cb(null, isMatch)
+    })
+}
+
+//The below code in user model will generate token as when the username & password matched and saved in mongodb as token, [user.generateToken] method then send the token as response to browser using cookie-parser.
+//making mathod --similarity [cb=(err, user)=>{}]
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //token is generated using user._id is default id stored in every registeration
+    var token = jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        //successfull in saving token
+        cb(null,user);
+    })
+}
 
 
 module.exports = mongoose.model('User',userSchema);
