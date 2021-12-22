267501caee0914268977ae73e7d62f4d33488018
Manish <vishwamanish@gmail.com>
2021-12-22 08:26:51 +0530

    Correcting errors and making use of postman correctly

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const User = require('./models/user');
+
+
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
 });
@@ -13,8 +20,24 @@ db.on('error',error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose'))
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
 app.get('/',(req,res)=>{
-    res.send('Hello world')
+    res.json({"hollo ~":"Hi ~~"})
 });
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userData)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        });
+    });
+    
+})
 
 app.listen(process.env.PORT || 3000)
